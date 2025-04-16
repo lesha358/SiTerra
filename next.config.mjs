0802057996379e33cba44c8d852d3a67984ffd9e
LittleBarista <chaplinrus@gmail.com>
2025-04-16 23:04:29 +0300

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,6 @@
 const nextConfig = {
   images: {
     domains: ['siterra-studio.ru', 'www.siterra-studio.ru'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/favicon.ico',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ]
   }
 }
 
