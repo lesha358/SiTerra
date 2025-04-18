378d958b778f5232d4e76c0b9077bbe4ee429aa1
LittleBarista <chaplinrus@gmail.com>
2025-04-18 11:44:45 +0300

    Добавлены заголовки для favicon.svg

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,19 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/favicon.svg',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+          {
+            key: 'Content-Type',
+            value: 'image/svg+xml',
+          },
+        ],
+      },
     ]
   }
 }
