40267bc33fda21989cc4a531c24d7d2d88c19a95
LittleBarista <chaplinrus@gmail.com>
2025-04-18 11:42:26 +0300

    Обновлена конфигурация favicon для использования SVG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: '/favicon.svg', type: 'image/svg+xml' },
       { url: '/icon', type: 'image/png' },
       { url: '/favicon.ico' }
     ],
